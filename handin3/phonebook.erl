6729e216d77aa9de2499c2b6dbf10843f4a7cf9b
Mads Ohm Larsen <mads.ohm@gmail.com>
2010-10-09 16:44:24 +0200

    Started on assignemt 3

---
+++ handin3/phonebook.erl
@@ -0,0 +1,62 @@
+%% Basic concurrent phone-book
+%%
+%% @author Ken Friis Larsen <kflarsen@diku.dk>
+%% @copyright Ken Friis Larsen 2009-2010
+
+-module(phonebook).
+-export([start/0, add/2, list_all/1, update/2]).
+
+%% Interface
+
+start() -> spawn(fun() -> loop(dict:new()) end).
+
+add(Pid, Contact) ->
+    rpc(Pid, {add, Contact}).
+
+list_all(Pid) ->
+    rpc(Pid, list_all).
+
+update(Pid, Contact) ->
+    rpc(Pid, {update, Contact}).
+
+
+%% Internal implementation
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+	{Pid, Response} ->
+	    Response
+    end.
+
+loop(Contacts) ->
+    receive
+	{From, Request} ->
+	    {Res, Updated} = handle_request(Request, Contacts),
+	    From ! {self(), Res},
+	    loop(Updated)
+    end.
+
+%% 
+handle_request(Request, Contacts) ->
+    case Request of
+        {add, Contact} -> 
+            {Name,_,_} = Contact,
+            case dict:is_key(Name, Contacts) of 
+                false -> {ok, dict:store(Name, Contact, Contacts)};
+                true  -> {{error, Name, is_already_there},
+                          Contacts}
+            end;
+        list_all ->
+            List = dict:to_list(Contacts),
+            {{ok, lists:map(fun({_, C}) -> C end, List)},
+             Contacts};
+        {update, Contact} ->
+	    {Name,_,_} = Contact,
+            {ok,
+             dict:store(Name, Contact, Contacts)};
+        Other ->
+            {{error,unknown_request, Other},
+             Contacts}
+    end.
+
